1cd94919805d69548cbcbecac5f7c620ed9c2144
鈴木悠斗 <yuto-suzuki@suzukiyuutonoMacBook-Pro.local>
2025-11-01 14:08:49 +0900

    Initial Commit

---
+++ WeatherApplicationDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherApplicationDemo
+//
+//  Created by 鈴木悠斗 on 2025/11/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherApplicationDemo/WeatherApplicationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApplicationDemoApp.swift
+//  WeatherApplicationDemo
+//
+//  Created by 鈴木悠斗 on 2025/11/01.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApplicationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
